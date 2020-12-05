4dfb754395869cac16216c94ec1b0c6501e1a91e
ShaoJenChen <cash3310@outlook.com>
2020-12-05 20:39:32 +0800

    1. Spacer 2. variable 3. optional map

--- SwiftUI-Repo/ContentView.swift
+++ SwiftUI-Repo/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
         VStack(alignment: .center, spacing: nil, content: {
             headerView()
             PricingView()
+            Spacer()
         })
         
 //        VStack {
@@ -100,66 +101,114 @@ struct ContentView_Previews: PreviewProvider {
 
 struct headerView: View {
     var body: some View {
-        VStack {
+        HStack {
             Text("Choose Plan")
+            Spacer()
         }
+        .padding()
     }
 }
 
 struct caseBasicView: View {
+    
+    var title: String
+    var price: String
+    var textColor: Color
+    var bgColor: Color
+    
     var body: some View {
         
         VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 1, content: {
             
-            Text("Basic")
+            Text(title)
                 .font(.title)
-                .foregroundColor(.white)
+                .foregroundColor(textColor)
                 .padding(.top, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-            Text("$9")
-                .foregroundColor(.white)
+            Text(price)
+                .foregroundColor(textColor)
                 .font(.system(size: 30, weight: .heavy, design: .monospaced))
             Text("per month")
                 .font(.body)
-                .foregroundColor(.white)
+                .foregroundColor(textColor)
                 .padding(.bottom, 10)
             
         })
         .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
         .padding(.all, 10)
-        .background(Color.purple)
+        .background(bgColor)
         .cornerRadius(10)
     }
 }
 struct caseProView: View {
+    
+    var title: String
+    var price: String
+    var textColor: Color
+    var bgColor: Color
+    var icon: String?
+    
     var body: some View {
         VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 1, content: {
             
-            Text("Preminun")
+            icon.map({
+                Image(systemName: $0)
+            })
+            .font(.largeTitle)
+            .foregroundColor(textColor)
+            
+            Text(title)
                 .font(.title)
-                .foregroundColor(.black)
+                .foregroundColor(textColor)
                 .padding(.top, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-            Text("$19")
-                .foregroundColor(.black)
+            Text(price)
+                .foregroundColor(textColor)
                 .font(.system(size: 30, weight: .heavy, design: .monospaced))
             Text("per month")
                 .font(.body)
-                .foregroundColor(.black)
+                .foregroundColor(textColor)
                 .padding(.bottom, 10)
             
         })
         .frame(minWidth: 0, maxWidth: .infinity, minHeight: 100)
         .padding(.all, 10)
-        .background(Color(red: 240/255, green: 240/255, blue: 240/255))
+        .background(bgColor)
         .cornerRadius(10)
     }
 }
 
 struct PricingView: View {
     var body: some View {
-        HStack(alignment: .center, spacing: 5, content: {
-            caseBasicView()
-            caseProView()
-        })
-        .padding([.leading, .trailing], /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            HStack(alignment: .center, spacing: 10, content: {
+                caseBasicView(title: "Basic", price: "$9", textColor: .white, bgColor: .purple)
+                ZStack {
+                    caseProView(title: "Pro", price: "$19", textColor: .black, bgColor: Color(red: 240/255, green: 240/255, blue: 240/255))
+                    Text("Best for designer")
+                        .font(.system(.caption, design: .rounded))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/, 5)
+                        .background(Color.yellow)
+                        .cornerRadius(10)
+                        .offset(x: 0, y: 65)
+                }
+            })
+            .padding(.all, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            
+            ZStack {
+                caseProView(title: "Team", price: "$299", textColor: .white, bgColor: Color.gray, icon: "wand.and.rays")
+                    .padding([.leading, .trailing], /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                Text("Perfect for teams")
+                    .font(.system(.caption, design: .rounded))
+                    .fontWeight(.heavy)
+                    .foregroundColor(.white)
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/, 5)
+                    .background(Color.yellow)
+                    .cornerRadius(10)
+                    .offset(x: 0, y: 90)
+            }
+        }
+        
     }
 }
