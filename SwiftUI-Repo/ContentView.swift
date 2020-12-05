457c67727771869e2544fa873dcd044c345871a0
ShaoJenChen <cash3310@outlook.com>
2020-12-05 14:40:49 +0800

    Initial Commit

---
+++ SwiftUI-Repo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Repo
+//
+//  Created by 陳劭任 on 2020/12/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Repo/SwiftUI_RepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_RepoApp.swift
+//  SwiftUI-Repo
+//
+//  Created by 陳劭任 on 2020/12/5.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_RepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
