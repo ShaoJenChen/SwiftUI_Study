884061ede95a50ce930986f545d46d9b9a809d72
ShaoJenChen <cash3310@outlook.com>
2020-12-12 19:42:01 +0800

    Ch11 Navigation and customize back button

--- SwiftUI-Repo/List_ForEach_Identify.swift
+++ SwiftUI-Repo/List_ForEach_Identify.swift
@@ -7,6 +7,29 @@
 
 import SwiftUI
 
+var restaurants = [ Restaurant(name: "Cafe Deadend", image: "cafedeadend"),
+               Restaurant(name: "Homei", image: "homei"),
+               Restaurant(name: "Teakha", image: "teakha"),
+               Restaurant(name: "Cafe Loisl", image: "cafeloisl"),
+               Restaurant(name: "Petite Oyster", image: "petiteoyster"),
+               Restaurant(name: "For Kee Restaurant", image: "forkeerestaurant"),
+               Restaurant(name: "Po's Atelier", image: "posatelier"),
+               Restaurant(name: "Bourke Street Bakery", image: "bourkestreetbakery"),
+               Restaurant(name: "Haigh's Chocolate", image: "haighschocolate"),
+               Restaurant(name: "Palomino Espresso", image: "palominoespresso"),
+               Restaurant(name: "Homei", image: "upstate"),
+               Restaurant(name: "Traif", image: "traif"),
+               Restaurant(name: "Graham Avenue Meats And Deli", image: "grahamavenuemeats"),
+               Restaurant(name: "Waffle & Wolf", image: "wafflewolf"),
+               Restaurant(name: "Five Leaves", image: "fiveleaves"),
+               Restaurant(name: "Cafe Lore", image: "cafelore"),
+               Restaurant(name: "Confessional", image: "confessional"),
+               Restaurant(name: "Barrafina", image: "barrafina"),
+               Restaurant(name: "Donostia", image: "donostia"),
+               Restaurant(name: "Royal Oak", image: "royaloak"),
+               Restaurant(name: "CASK Pub and Kitchen", image: "caskpubkitchen")
+]
+
 struct Restaurant: Identifiable {
     var id = UUID()
     var name: String
@@ -15,28 +38,7 @@ struct Restaurant: Identifiable {
 
 
 struct List_ForEach_Identify: View {
-    var restaurants = [ Restaurant(name: "Cafe Deadend", image: "cafedeadend"),
-                   Restaurant(name: "Homei", image: "homei"),
-                   Restaurant(name: "Teakha", image: "teakha"),
-                   Restaurant(name: "Cafe Loisl", image: "cafeloisl"),
-                   Restaurant(name: "Petite Oyster", image: "petiteoyster"),
-                   Restaurant(name: "For Kee Restaurant", image: "forkeerestaurant"),
-                   Restaurant(name: "Po's Atelier", image: "posatelier"),
-                   Restaurant(name: "Bourke Street Bakery", image: "bourkestreetbakery"),
-                   Restaurant(name: "Haigh's Chocolate", image: "haighschocolate"),
-                   Restaurant(name: "Palomino Espresso", image: "palominoespresso"),
-                   Restaurant(name: "Homei", image: "upstate"),
-                   Restaurant(name: "Traif", image: "traif"),
-                   Restaurant(name: "Graham Avenue Meats And Deli", image: "grahamavenuemeats"),
-                   Restaurant(name: "Waffle & Wolf", image: "wafflewolf"),
-                   Restaurant(name: "Five Leaves", image: "fiveleaves"),
-                   Restaurant(name: "Cafe Lore", image: "cafelore"),
-                   Restaurant(name: "Confessional", image: "confessional"),
-                   Restaurant(name: "Barrafina", image: "barrafina"),
-                   Restaurant(name: "Donostia", image: "donostia"),
-                   Restaurant(name: "Royal Oak", image: "royaloak"),
-                   Restaurant(name: "CASK Pub and Kitchen", image: "caskpubkitchen")
-    ]
+    
 //    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
 //
 //    var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkeerestaurant", "posatelier", "bourkestreetbakery", "haighschocolate", "palominoespresso", "upstate", "traif", "grahamavenuemeats", "wafflewolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "caskpubkitchen"]
@@ -63,13 +65,13 @@ struct List_ForEach_Identify: View {
         
         List(restaurants.indices) { index in
             if 0 ... 1 ~= index {
-                FullImageRow(restaurant: self.restaurants[index])
+                FullImageRow(restaurant: restaurants[index])
             }
             else {
 //                BasicImageRow(restaurant: self.restaurants[index])
-                CardView(imageName: self.restaurants[index].image,
+                CardView(imageName: restaurants[index].image,
                          category: "GeoVision",
-                         heading: self.restaurants[index].name,
+                         heading: restaurants[index].name,
                          author: "SJ")
             }
         }
---
+++ SwiftUI-Repo/Navigation.swift
@@ -0,0 +1,209 @@
+//
+//  Navigation.swift
+//  SwiftUI-Repo
+//
+//  Created by 陳劭任 on 2020/12/12.
+//
+
+import SwiftUI
+
+struct Navigation: View {
+    
+    init() {
+//        let navBarAppearance = UINavigationBarAppearance()
+//        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.systemRed, .font: UIFont(name: "ArialRoundedMTBold", size: 35)!]
+//        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.systemRed, .font: UIFont(name: "ArialRoundedMTBold", size: 20)!]
+//
+//        UINavigationBar.appearance().standardAppearance = navBarAppearance
+//        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
+//        UINavigationBar.appearance().compactAppearance = navBarAppearance
+        
+//        navBarAppearance
+//            .setBackIndicatorImage(
+//                UIImage(systemName: "arrow.turn.up.left"),
+//                transitionMaskImage:
+//                    UIImage(systemName: "arrow.turn.up.left"))
+//        UINavigationBar.appearance().tintColor = .black
+    }
+    
+    var body: some View {
+        
+        //        NavigationView(content: {
+        //            NavigationLink(destination:
+        //                List_ForEach_Identify()
+        //                ) { /*@START_MENU_TOKEN@*/Text("Navigate")/*@END_MENU_TOKEN@*/ }
+//        })
+        
+        NavigationView {
+            
+            List {
+//                ForEach(restaurants) { restaurant in
+//
+//                    NavigationLink(
+//                        destination: RestaurantDetailView(restaurant: restaurant),
+//                        label: {
+//                            FullImageRow(restaurant: restaurant)
+//                        })
+//
+////                    FullImageRow(restaurant: restaurant)
+//                }
+                
+//                ForEach(articles) { article in
+//
+//                    NavigationLink(
+//                        destination: ArticleDetailView(article: article),
+//                        label: {
+//                            ArticleRow(article: article)
+//                        })
+//
+//
+//                }
+                
+                ForEach(articles) { article in
+                    
+                    ZStack {
+                        NavigationLink(destination: ArticleDetailView(article: article)) {
+                            EmptyView()
+                        }
+                        ArticleRow(article: article)
+                        
+                    }
+                    
+                }
+            }
+//            .navigationBarTitle("", displayMode: .inline)
+            .navigationBarHidden(true)
+            
+        }
+        
+    }
+}
+
+struct ArticleDetailView: View {
+    
+    @Environment(\.presentationMode) var mode
+    
+    var article: Article
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading) {
+                Image(article.image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                Group {
+                    Text(article.title)
+                        .font(.system(.title, design: .rounded))
+                        .fontWeight(.black)
+                        .lineLimit(3)
+                    
+                    Text("By \(article.author)".uppercased())
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                }
+                .padding(.bottom, 0)
+                .padding(.horizontal)
+                
+                Text(article.content)
+                    .font(.body)
+                    .padding()
+                    .lineLimit(1000)
+                    .multilineTextAlignment(.leading)
+            }
+        }
+        .edgesIgnoringSafeArea(.top)
+        .navigationBarBackButtonHidden(true)
+        //        .navigationBarHidden(false)
+        .navigationBarItems(leading:
+                                Button(action: {
+                                    self.mode.wrappedValue.dismiss()
+                                }, label: {
+                                    Image(systemName: "chevron.left.circle.fill")
+                                        .font(.largeTitle)
+                                        .foregroundColor(.white)
+                                })
+        )
+        
+    }
+
+}
+
+
+struct RestaurantDetailView: View {
+    
+    @Environment(\.presentationMode) var mode
+
+    var restaurant: Restaurant
+
+    var body: some View {
+        VStack {
+            Image(restaurant.image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+
+            Text(restaurant.name)
+                .font(.system(.title, design: .rounded))
+                .fontWeight(.black)
+
+            Spacer()
+        }
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading:
+                                Button(action: {
+                                    self.mode.wrappedValue.dismiss()
+                                }, label: {
+                                    Text("\(Image(systemName: "chevron.left")) \(restaurant.name)")
+                                })
+//                                Button("Back", action: {
+//                                    self.mode.wrappedValue.dismiss()
+//                                })
+        )
+
+    }
+}
+
+struct ArticleRow: View {
+    
+    var article: Article
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Image(article.image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(5)
+            
+            Text(article.title)
+                .font(.system(.title, design: .rounded))
+                .fontWeight(.black)
+                .lineLimit(3)
+                .padding(.bottom, 0)
+            
+            Text("By \(article.author)".uppercased())
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                .padding(.bottom, 0)
+            
+            HStack(spacing: 3) {
+                ForEach(1...(article.rating), id: \.self) { _ in
+                    Image(systemName: "star.fill")
+                        .font(.caption)
+                        .foregroundColor(.yellow)
+                }
+            }
+            
+            Text(article.excerpt)
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+            
+        }
+    }
+}
+
+
+struct Navigation_Previews: PreviewProvider {
+    static var previews: some View {
+        Navigation()
+    }
+}
